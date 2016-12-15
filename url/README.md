83e10fd30a53cecc301f6a248ca0024cf36c2663
Kevin Sawicki <kevinsawicki@gmail.com>
2016-12-15 08:40:30 -0800

    Add simple URL loader app

---
+++ url/README.md
@@ -0,0 +1,12 @@
+# URL
+
+Simple [Electron](http://electron.atom.io) application that loads a URL
+passed on the command line in a window.
+
+## Getting started
+
+- Install [Node LTS](https://nodejs.org)
+- Clone this repository
+- `cd price`
+- `npm install` to install the application's dependencies
+- `npm start https://github.com` to start the application and load GitHub
---
+++ url/app.js
@@ -0,0 +1,30 @@
+var electron = require('electron') // http://electron.atom.io/docs/api
+
+var window = null
+
+// Wait until the app is ready
+electron.app.once('ready', function () {
+  // Create a new window
+  window = new electron.BrowserWindow({
+    // Set the initial width to 800px
+    width: 800,
+    // Set the initial height to 600px
+    height: 600,
+    // Don't show the window until it ready, this prevents any white flickering
+    show: false,
+    webPrefences: {
+      // Disable node integration in remote page
+      nodeIntegration: false
+    }
+  })
+
+  // URL is argument to npm start
+  var url = process.argv[2]
+  window.loadURL(url)
+
+  // Show window when page is ready
+  window.once('ready-to-show', function () {
+    window.maximize()
+    window.show()
+  })
+})
