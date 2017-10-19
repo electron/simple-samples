f391b1d02a6feae72dbdee6877fecc7be72ce097
Shelley Vohr <shelley.vohr@gmail.com>
2017-10-18 21:20:10 -0400

    mirros updates complete

--- mirror/window.js
+++ mirror/window.js
@@ -1,5 +1,5 @@
 // Run this function after the page has loaded
-$(function() {
+$(() => {
   const electron = require('electron')
 
   const display = electron.screen.getPrimaryDisplay() // http://electron.atom.io/docs/api/screen
@@ -15,16 +15,19 @@ $(function() {
     }
   }
 
-  navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
+  navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
     const video = document.querySelector('video')
     video.srcObject = stream  // Play stream in <video> element
-  }).catch(function(error) {
+  }).catch((error) => {
     console.error(error)
   })
 
   // Listen for the Rainbow Filter checkbox being clicked
-  $("#rainbow-toggle").change(() => {
-    (this.checked) ? $('.rainbow-filter').show() : $('.rainbow-filter').hide()
+  $("#rainbow-toggle").on('change', () => {
+    if ($('#rainbow-toggle').is(':checked')) {
+      $('.rainbow-filter').show()
+    } else {
+      $('.rainbow-filter').hide()
+    }
   })
-
 })
