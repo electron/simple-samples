788cab48a7c16c98160e94cce5652f26c0488c38
Łukasz Barczyński <lukasz.barczynski@outlook.com>
2018-04-15 01:05:59 +0200

    Real-time update chart for activity-monitor project

--- activity-monitor/window.js
+++ activity-monitor/window.js
@@ -1,18 +1,21 @@
-$(() => {
-  const os = require('os')
+const os = require('os')
+var chart = null;
+var lastMeasureTimes = [];
 
+function setLastMeasureTimes(cpus) {
+  for (let i = 0; i < cpus.length; i++) {
+    lastMeasureTimes[i] = getCpuTimes(cpus[i]);
+  }
+}
+
+function getDatasets() {
   const datasets = []
   const cpus = os.cpus()
 
   for (let i = 0; i < cpus.length; i++) {
     const cpu = cpus[i]
-
     const cpuData = {
-      data: [
-        cpu.times.user,
-        cpu.times.sys,
-        cpu.times.idle,
-      ],
+      data: getCpuTimes(cpu),
       backgroundColor: [
         'rgba(255, 99, 132, 1)',
         'rgba(54, 162, 235, 1)',
@@ -21,8 +24,33 @@ $(() => {
     }
     datasets.push(cpuData)
   }
+  testCpus = os.cpus();
+  return datasets;
+}
 
-  var chart = new Chart($('.chart'), {
+function updateDatasets() {
+  const cpus = os.cpus()
+  for (let i = 0; i < cpus.length; i++) {
+    const cpu = cpus[i]
+    chart.data.datasets[i].data = getCpuTimes(cpu);
+    chart.data.datasets[i].data[0] -= lastMeasureTimes[i][0];
+    chart.data.datasets[i].data[1] -= lastMeasureTimes[i][1];
+    chart.data.datasets[i].data[2] -= lastMeasureTimes[i][2];
+  }
+  chart.update();
+  setLastMeasureTimes(cpus);
+}
+
+function getCpuTimes(cpu) {
+  return [
+    cpu.times.user,
+    cpu.times.sys,
+    cpu.times.idle,
+  ];
+}
+
+function drawChart() {
+  chart = new Chart($('.chart'), {
     type: 'doughnut',
     data: {
       labels: [
@@ -30,7 +58,7 @@ $(() => {
         'System Time (ms)',
         'Idle Time (ms)'
       ],
-      datasets: datasets
+      datasets: getDatasets()
     },
     options: {
       maintainAspectRatio: false,
@@ -48,6 +76,12 @@ $(() => {
         }
       }
     }
-  })
+  });
 
+  setInterval(updateDatasets, 1000);
+}
+
+$(() => {
+  setLastMeasureTimes(os.cpus());
+  drawChart();
 })
