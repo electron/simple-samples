d52481342469771384f9af163dace4ebcb94c0c5
ktmouk <ktmouk@gmail.com>
2019-11-19 22:33:56 +0900

    notarizeを追加

---
+++ build/notarize.js
@@ -0,0 +1,31 @@
+const path = require('path')
+const { notarize } = require('electron-notarize')
+
+// TravisCIの環境変数 (process.env) から取得
+const appleId = process.env.APPLE_ID
+const appleIdPassword = process.env.APPLE_PASSWORD
+
+// .appのパスを設定
+const appPath = path.resolve(__dirname, '../build/mac/Example.app')
+
+// package.jsonからappIdを取得
+const configPath = path.resolve(__dirname, '../package.json')
+const config = require(configPath)
+const appBundleId = config.build.appId
+
+exports.default = async () => {
+  // appleId, appleIdPasswordが取得できない場合はスキップ
+  // ブランチがmasterでない場合はスキップ。例えばプルリクエストのブランチなど
+  if (!appleId || !appleIdPassword || process.env.BRANCH !== 'master') {
+    console.warn('notarize skipped');
+    return;
+  }
+
+  // notarizeする
+  await notarize({
+    appBundleId,
+    appPath,
+    appleId,
+    appleIdPassword,
+  })
+}
